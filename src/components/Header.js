0399e4aa1215dd914cbaee0b58b2bbbcf7c6267c
YakirHasid <hasidyakir@gmail.com>
2023-09-30 12:20:39 +0300

    created Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import logo from './assets/investment-calculator-logo.png';
+
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <header className="header">
+      <img src={logo} alt="logo" />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+};
+
+export default Header;
