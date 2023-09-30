5c35b2187c2f36dbe941a13e0ca6ba4a0478803a
YakirHasid <hasidyakir@gmail.com>
2023-09-30 14:30:33 +0300

    two-way binding for each input

--- src/components/InvestmentForm/InvestmentForm.js
+++ src/components/InvestmentForm/InvestmentForm.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const InvestmentForm = (props) => {
+  const [currentSavings, setCurrentSavings] = useState("");
+  const [yearlySavings, setYearlySavings] = useState("");
+  const [expectedIntereset, setExpectedIntereset] = useState("");
+  const [investmentDuration, setInvestmentDuration] = useState("");
+
   return (
     <form className="form">
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input
+            type="number"
+            id="current-savings"
+            value={currentSavings}
+            onChange={e => setCurrentSavings(e.target.value)}
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            type="number"
+            id="yearly-contribution"
+            value={yearlySavings}
+            onChange={e => setYearlySavings(e.target.value)}
+          />
         </p>
       </div>
       <div className="input-group">
@@ -18,11 +33,21 @@ const InvestmentForm = (props) => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input
+            type="number"
+            id="expected-return"
+            value={expectedIntereset}
+            onChange={e => setExpectedIntereset(e.target.value)}
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input
+            type="number"
+            id="duration"
+            value={investmentDuration}
+            onChange={e => setInvestmentDuration(e.target.value)}
+          />
         </p>
       </div>
       <p className="actions">
